cdac0076e96f508ae8ecf525d6f9996e4fd6c43c
akhr1997 <ak.hr1997@gmail.com>
2022-10-07 17:06:33 -0500

    promise from JSON file

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-let p = new Promise((resolve, reject) => {
-    let sum = 1 + 2;
-    if(sum == 2){
-        resolve("correct")
-    }else{
-        reject("wrong")
-    }
-})
+// let p = new Promise((resolve, reject) => {
+//     let sum = 1 + 2;
+//     if(sum == 2){
+//         resolve("correct")
+//     }else{
+//         reject("wrong")
+//     }
+// })
 
-p.then((message) => {
-    console.log("This is in the then - " +message)
-}).catch((message) => {
-    console.log("This is in the catch - " +message)
-})
+// p.then((message) => {
+//     console.log("This is in the then - " +message)
+// }).catch((message) => {
+//     console.log("This is in the catch - " +message)
+// })
+
+// ----------------fetch using json file-----------------------
+
+// function jFetch(){
+//     fetch("index.json")
+        // .then((response) => {console.log(response)})
+// }
+
+// jFetch();
+
+// ----------------fetch using json file-----------------------
+
+function jFetch(){
+    fetch("index.json")
+        .then((response) => response.json())
+        .then((data) => {console.log(data)})
+}
+
+jFetch();
